b8f453f227b2c378cc053fd9d1941c7d015bd246
Darun Karas <darunkaras@Daruns-MacBook-Air.local>
2024-12-23 14:44:30 +0600

    prop validation

--- src/pages/AllBlogs/Blog.jsx
+++ src/pages/AllBlogs/Blog.jsx
@@ -1,11 +1,12 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 import { FaBookOpenReader } from "react-icons/fa6";
 import { FaHeart } from "react-icons/fa";
+import PropTypes from "prop-types";
 
 export default function Blog({ blog }) {
-    const { imageUrl, title, shortDescription, category } = blog
+    const { imageUrl, title, shortDescription, category } = blog;
     return (
-        <div className=" bg-white rounded-sm shadow-lg overflow-hidden font-lato">
+        <div className="bg-white rounded-sm shadow-lg overflow-hidden font-lato">
             {/* Blog Image */}
             <img src={imageUrl} alt="Blog Image" className="w-full h-64 object-cover" />
 
@@ -18,12 +19,31 @@ export default function Blog({ blog }) {
                 <p className="text-gray-600 mt-2">{shortDescription}..</p>
 
                 <div className="flex justify-between items-center mt-4">
-                    <Link className="text-sm text-[#b28b51] hover:text-black flex items-center gap-1"><FaBookOpenReader /> Read More</Link>
-                    <button className="text-sm text-[#b28b51] hover:text-black flex items-center gap-1">
-                        <FaHeart />  Wish List
+                    <Link
+                        className="text-sm text-[#b28b51] hover:text-black flex items-center gap-1"
+                        to={`/blog/${blog.id}`}
+                    >
+                        <FaBookOpenReader /> Read More
+                    </Link>
+                    <button
+                        type="button"
+                        className="text-sm text-[#b28b51] hover:text-black flex items-center gap-1"
+                    >
+                        <FaHeart /> Wish List
                     </button>
                 </div>
             </div>
         </div>
-    )
+    );
 }
+
+// Prop Validation
+Blog.propTypes = {
+    blog: PropTypes.shape({
+        imageUrl: PropTypes.string.isRequired,
+        title: PropTypes.string.isRequired,
+        shortDescription: PropTypes.string.isRequired,
+        category: PropTypes.string.isRequired,
+        id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+    }).isRequired,
+};
