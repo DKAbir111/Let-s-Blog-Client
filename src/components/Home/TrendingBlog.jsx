fffc83b4ece265b1aa215a2f7bbf40b9a519a725
Darun Karas <darunkaras@Daruns-MacBook-Air.local>
2024-12-26 01:21:12 +0600

    add extra two component in home page

---
+++ src/components/Home/TrendingBlog.jsx
@@ -0,0 +1,52 @@
+import Marquee from "react-fast-marquee";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+export default function TrendingBlogs() {
+    const [topPosts, setTopPosts] = useState([]);
+
+    // Fetch the top posts from the API
+    useEffect(() => {
+        fetch("http://localhost:5001/api/top-posts")
+            .then((response) => response.json())
+            .then((data) => setTopPosts(data))
+            .catch((error) => console.error("Error fetching data: ", error));
+    }, []);
+
+    return (
+        <div className="my-12 container mx-auto">
+            <h3 className="text-2xl text-center text-gray-800 font-semibold mb-6">
+                Trending Blogs
+            </h3>
+            <Marquee pauseOnHover gradient={false} speed={50}>
+                {topPosts.map((blog) => (
+                    <div
+                        key={blog._id}
+                        className="flex flex-col items-center justify-center mx-6"
+                    >
+                        <img
+                            src={blog.imageUrl}
+                            alt={blog.title}
+                            className="w-36 h-36 object-cover rounded-lg mb-2"
+                        />
+                        <div className="text-center">
+                            <h4 className="text-sm font-semibold text-gray-700 mb-2">
+                                {blog.title}
+                            </h4>
+                            <p className="text-xs text-gray-500 mb-2">{blog.category}</p>
+                            <p className="text-xs text-gray-400 mb-4">
+                                {blog.shortDescription}
+                            </p>
+                            <Link
+                                to={`/blog/${blog._id}`}
+                                className="text-sm text-[#b28b51] hover:underline"
+                            >
+                                View Details
+                            </Link>
+                        </div>
+                    </div>
+                ))}
+            </Marquee>
+        </div>
+    );
+}
--- src/layout/HomeLayout.jsx
+++ src/layout/HomeLayout.jsx
@@ -1,6 +1,7 @@
 import Banner from "../components/Home/Banner";
 import Newsletter from "../components/Home/NewsLetter";
 import RecentBlogs from "../components/Home/RecentBlogs";
+import TrendingBlog from "../components/Home/TrendingBlog";
 import VividImpressions from "../components/Home/VividImpressions";
 
 
@@ -9,6 +10,7 @@ export default function HomeLayout() {
         <div>
             <Banner />
             <RecentBlogs />
+            <TrendingBlog />
             <VividImpressions />
             <Newsletter />
 
